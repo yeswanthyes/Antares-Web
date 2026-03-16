f9af4396794e4e3c3fac09bef59d118c328afce5
yeswanthyes <yesyeswanth232@gmail.com>
2026-03-16 22:14:30 +0530

    on production

--- frontend/api/waitlist.js
+++ frontend/api/waitlist.js
@@ -111,6 +111,7 @@ export default async function handler(req, res) {
         ],
       };
 
+      console.log('Sending email to:', email);
       await transporter.sendMail(mailOptions);
       return res.status(200).json({ message: 'Success! You are on the list.' });
     } else {
